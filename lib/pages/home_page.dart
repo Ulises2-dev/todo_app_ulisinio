dd794e0a0e58b846fadf4cec974d2a1bbab73a32
Ulises Millan <millan_19@icloud.com>
2024-11-13 22:56:45 -0600

    Implementación de funciones y añadir tareas

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:todo_app_ulisinio/utils/todo_list.dart';
 
 class HomePage extends StatefulWidget {
-   HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final _controller = TextEditingController();
   List toDoList = [
-    ['Leer la documentación oficial', false],
+    ['Pasar la unidad 2 de Móviles', false],
     ['Tomar agua', false],
     ['Hacer mis Commits', false]
   ];
@@ -21,24 +23,75 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void saveNewTask () {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+  }
+
+  void deleteTask (int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple.shade300,
       appBar: AppBar(
-        title: const Text(
-          'ToDo Simple',
-          ),
-          backgroundColor: Colors.deepPurple,
-          foregroundColor: Colors.white,
+        title: const Text('ToDo Simple'),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
       ),
-      body: ListView.builder(itemCount: toDoList.length, itemBuilder: (BuildContext context, index) {
-        return TodoList(
-          taskName: toDoList[index][0],
-          taskCompleted: toDoList[index][1],
-          onChanged: (value) => checkBoxChanged(index),
-        );
-      }),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return TodoList(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(index),
+            deleteFunction: (context) => deleteTask(index),
+          );
+        },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: 'Añadir nueva tarea',
+                    filled: true,
+                    fillColor: Colors.deepPurple.shade200,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.deepPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.deepPurple,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: saveNewTask,
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
